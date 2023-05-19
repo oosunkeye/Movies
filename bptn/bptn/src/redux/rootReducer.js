aa37178718115a5e8a38dd112cd3d140ef7f3c72
Oladapo Osunkeye <davidosunkeye@gmail.com>
2023-05-18 20:03:52 -0400

    Delete bptn/bptn directory

--- bptn/bptn/src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-// ** Reducers Imports
-import likedMovies from './stores/likedMovies'
-
-const rootReducer = {
-    likedMovies
-}
-
-export default rootReducer
--- bptn/bptn/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-// ** Redux Imports
-import rootReducer from './rootReducer'
-import { configureStore } from '@reduxjs/toolkit'
-
-const store = configureStore({
-    reducer: rootReducer
-})
-
-export { store }
